004bade1dd8cded2d68b7a1f91f1c17383d4a7d1
Jin Glad <jihanchowdhury70@gmail.com>
2020-10-15 17:43:43 +0600

    almost done

--- index.js
+++ index.js
@@ -25,16 +25,17 @@ client.connect(err => {
     const serviceCollection = client.db(process.env.DB_NAME).collection("services");
     const ordersCollection = client.db(process.env.DB_NAME).collection("ordersCollection");
     const reviewsCollection = client.db(process.env.DB_NAME).collection("reviews");
+    const adminsCollection = client.db(process.env.DB_NAME).collection("admins");
 
-    app.post('/addService', (req, res) => {
-        const service = req.body;
-        // console.log(service);
-        serviceCollection.insertMany(service)
-            .then(result => {
-                console.log(result.insertedCount);
-                res.send(result.insertedCount);
-            })
-    })
+    // app.post('/addService', (req, res) => {
+    //     const service = req.body;
+    //     // console.log(service);
+    //     serviceCollection.insertMany(service)
+    //         .then(result => {
+    //             console.log(result.insertedCount);
+    //             res.send(result.insertedCount);
+    //         })
+    // })
 
     app.get('/services', (req, res) => {
         serviceCollection.find({})
@@ -43,7 +44,7 @@ client.connect(err => {
             })
     })
 
-    app.post('/postOrder',(req,res) => {
+    app.post('/postOrder', (req, res) => {
         const file = req.files.file;
         const name = req.body.name;
         const email = req.body.email;
@@ -55,10 +56,10 @@ client.connect(err => {
         const encImg = newImg.toString('base64');
 
 
-        file.mv(`${__dirname}/ordersPic/${file.name}`,err => {
-            if(err){
+        file.mv(`${__dirname}/ordersPic/${file.name}`, err => {
+            if (err) {
                 console.log(err);
-                return res.send(500).send({msg: 'Failed to upload Image'});
+                return res.send(500).send({ msg: 'Failed to upload Image' });
             }
         })
 
@@ -67,8 +68,41 @@ client.connect(err => {
             size: file.size,
             img: Buffer.from(encImg, 'base64')
         };
+        ordersCollection.insertOne({ name, email, description, price, service, image, serviceIcon })
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
 
-        ordersCollection.insertOne({name,email,description,price,service,image,serviceIcon})
+    app.post('/addService', (req, res) => {
+        const file = req.files.file;
+        const description = req.body.description;
+        const title = req.body.title;
+        const newImg = file.data;
+        const encImg = newImg.toString('base64');
+
+
+        file.mv(`${__dirname}/ordersPic/${file.name}`, err => {
+            if (err) {
+                console.log(err);
+                return res.send(500).send({ msg: 'Failed to upload Image' });
+            }
+        })
+
+        var image = {
+            contentType: file.mimetype,
+            size: file.size,
+            img: Buffer.from(encImg, 'base64')
+        };
+        serviceCollection.insertOne({ description, title, image })
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
+
+    app.post('/addAdmin', (req, res) => {
+        let admin = req.body;
+        adminsCollection.insertOne(admin)
             .then(result => {
                 res.send(result.insertedCount > 0);
             })
@@ -76,6 +110,13 @@ client.connect(err => {
 
     app.get('/customerOrders', (req, res) => {
         ordersCollection.find({ email: req.query.email })
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.get("/fullOrderList", (req, res) => {
+        ordersCollection.find({})
             .toArray((err, documents) => {
                 res.send(documents);
             })
@@ -93,6 +134,14 @@ client.connect(err => {
         reviewsCollection.find({})
             .toArray((err, documents) => {
                 res.send(documents);
+            })
+    })
+
+    app.post('/isAdmin', (req, res) => {
+        const email = req.body.email;
+        adminsCollection.find({ email: email })
+            .toArray((err, admins) => {
+                res.send(admins.length > 0);
             })
     })
 });
